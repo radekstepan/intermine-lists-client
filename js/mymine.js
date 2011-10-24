98653bbcb79760030f4f4d6a195df5368f6c8f0a
Radek Stepan <radek.stepan@gmail.com>
2011-10-24 16:30:15 +0100

    row items selectors

--- js/mymine.js
+++ js/mymine.js
@@ -11,7 +11,40 @@
 var MyMine = (function() {
 
     /** @string selector for all folder names in the main table */
-    var selectAllFolderNames = 'table#lists tbody tr.folder h2.name';
+    var selectAllFolderNames  = 'table#lists tbody tr.folder h2.name',
+    /** @string selector for all checked checkboxes in the main table */
+        selectAllCheckedItems = 'table#lists tbody tr input[type="checkbox"].check:checked',
+    /** @string selector for item name from main table row */
+        selectRowToItemName   = 'h2.name';
+
+    /**
+     * Get all folders from the table after it was initialized
+     * @XXX rewrite to fetch directly from source instead of parsing
+     */
+    function privateIntitializeFolders() {
+        $(selectAllFolderNames).each(function() {
+            MyMine.model.addFolder($(this).text());
+        });
+    }
+
+    /**
+     * Attach appropriate classes to items that have been selected (and remembered) after reload
+     * @XXX rewrite to fetch directly from source instead of parsing
+     */
+    function privateInitilizeSelectedItems() {
+        $(selectAllCheckedItems).each(function() {
+            var row = $(this).closest('tr');
+            // set selected class
+            row.addClass('selected');
+            // set the item as selected internally
+            MyMine.presenter.setAsSelectedItem(
+                row.find(selectRowToItemName).text(),
+                (row.hasClass('list')) ? 'list' : 'folder');
+        });
+
+        // based on selected items, update toolbar
+        MyMine.presenter.updateToolbar();
+    }
 
     return {
 
@@ -19,10 +52,14 @@ var MyMine = (function() {
          * Initialize data after document is ready
          */
         init: function() {
-            // XXX get all the folders we have
-            $(selectAllFolderNames).each(function() {
-                MyMine.model.addFolder($(this).text());
-            });
+            // initialize all folders
+            privateIntitializeFolders();
+
+            // initialize selected items
+            privateInitilizeSelectedItems();
+
+            // initialize item selection handlers
+            MyMine.presenter.initializeHandlers();
         },
         
         /********************* Model *********************/
@@ -32,12 +69,11 @@ var MyMine = (function() {
             /** @dict internal 'folders' storage */
             var folders = {};
 
-            /** @string message */
-            var messageListExists = "Wowza! This list already exists.";
+            /** @dict internal 'lists' storage */
+            var lists = {};
 
-            function privateAddFolder() {
-                // do stuff here
-            }
+            /** @string messages */
+            var messageListExists = "Wowza! This list already exists.";
 
             return {
 
@@ -46,7 +82,7 @@ var MyMine = (function() {
                  * @throws
                  */
                 addFolder: function(name) {
-                    if (!MyMine.model.existsFolder(name)) {
+                    if (! MyMine.model.existsFolder(name)) {
                         folders[name] = true;
                     } else {
                         throw messageListExists;
@@ -69,20 +105,122 @@ var MyMine = (function() {
 
         'presenter': (function() {
 
-            var private_var;
+            /** @string selector for toolbar buttons */
+            var selectToolbarButtons  = 'div#toolbar div.btn',
+            /** @string selector for all main table checkboxes */
+                selectAllCheckboxes    = 'table#lists tbody tr input[type="checkbox"].check',
+            /** @string selector for all main table rows */
+                selectAllRows    = 'table#lists tbody tr',
+            /** @string selector for row checkbox from main table row */
+                selectRowToCheckbox = 'input[type="checkbox"].check';
 
-            function private_method() {
-                // do stuff here
+            /** @dict selected item's name => type in the main table */
+            var selected      = {},
+            /** @int number of selected items in the main table */
+                selectedCount = 0;
+
+
+            /**
+             * Initialize handlers for selecting items in the main table
+             */
+            function initializeSelectItemHandlers() {
+                // on checkbox click
+                $(selectAllCheckboxes).click(function(e) {
+                    MyMine.presenter.selectRow(e, this);
+                });
+
+                // on row select
+                $(selectAllRows).click(function(e) {
+                    var checkbox = $(this).find(selectRowToCheckbox);
+                    // first tick the checkbox
+                    if (checkbox.attr('checked')) {
+                        checkbox.attr('checked', false);
+                    } else {
+                        checkbox.attr('checked', 'checked');
+                    }
+                    MyMine.presenter.selectRow(e, checkbox);
+                });
             }
 
             return {
 
-                method_1: function() {
-                    return "presenter first";
+                /**
+                 * Initialize all the handlers
+                 */
+                initializeHandlers: function() {
+                    initializeSelectItemHandlers();
                 },
 
-                method_2: function() {
-                    return "presenter second";
+                /**
+                 * Set item in the main table as selected
+                 */
+                setAsSelectedItem: function(name, type) {
+                    selected[name] = type;
+                    selectedCount++;
+                },
+
+                /**
+                 * Add Folder/Organize lists switch
+                 */
+                updateToolbar: function() {
+                    if (selectedCount > 0) {
+                        // organize lists
+                        $(selectToolbarButtons + '.folder').hide();
+                        $(selectToolbarButtons + '.organize').show();
+                    } else {
+                        // create folder
+                        $(selectToolbarButtons + '.folder').show();
+                        $(selectToolbarButtons + '.organize').hide();
+                    }
+                },
+
+                /**
+                 * Select row on checkbox or row click
+                 */
+                selectRow: function(e, element) {
+                    var row = $(element).closest('tr');
+                    // folder?
+                    if (row.hasClass('folder')) {
+                        var inFolder = true;
+                        if ($(element).is(':checked')) {
+                            while (inFolder) {
+                                row = row.next();
+                                if (row.hasClass('list') && row.hasClass('foldered')) {
+                                    if (! row.hasClass('selected')) {
+                                        row.addClass('selected');
+                                        row.find(selectRowToCheckbox).attr('checked', 'checked');
+                                        selectedCount++;
+                                    }
+                                } else {
+                                    inFolder = false;
+                                }
+                            }
+                        } else {
+                            while (inFolder) {
+                                row = row.next();
+                                if (row.hasClass('list') && row.hasClass('foldered')) {
+                                    if (row.hasClass('selected')) {
+                                        row.removeClass('selected');
+                                        row.find(selectRowToCheckbox).attr('checked', false);
+                                        selectedCount--;
+                                    }
+                                } else {
+                                    inFolder = false;
+                                }
+                            }
+                        }
+                    } else {
+                        if (! $(element).is(':checked')) {
+                            row.removeClass('selected');
+                            selectedCount--;
+                        } else {
+                            row.addClass('selected');
+                            selectedCount++;
+                        }
+                    }
+
+                    MyMine.presenter.updateToolbar();
+                    e.stopPropagation();
                 }
             };
         })()
