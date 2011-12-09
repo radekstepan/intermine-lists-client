d6e8b322a5bb75bce82c2e88906bf42fecc4a44c
Radek Stepan <radek.stepan@gmail.com>
2011-12-09 17:45:04 +0000

    use bindAll to use this

--- fundamental.js/js/views/ListCollectionView.js
+++ fundamental.js/js/views/ListCollectionView.js
@@ -6,6 +6,8 @@ App.Views.ListCollectionView = Backbone.View.extend({
 	"el": "table#table tbody",
 
 	initialize: function(options) {
+		_.bindAll(this, "addOneList");
+
 		// On initialization, add all existing list items.
 		this.addAllLists();
 	},
@@ -14,7 +16,7 @@ App.Views.ListCollectionView = Backbone.View.extend({
 	// appending its element to the `<tbody>`.
 	addOneList: function(list) {
 		var view = new App.Views.ListItemView({model: list});
-		$("table#table tbody").append(view.render().el); // TODO fix this hack
+		$(this.el).append(view.render().el); // TODO fix this hack
 	},
 
 	// Add all items in the Lists collection at once.
