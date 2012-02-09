4aa384e6bcbba5a86b83ed6789d98ff0c54f5c30
Radek Stepan <radek.stepan@gmail.com>
2012-02-09 09:14:54 +0000

    cleanup

--- fundamental.js/compile.sh
+++ fundamental.js/compile.sh
@@ -1,4 +1,4 @@
-coffee --compile --output test/ src/
+coffee --compile --output js/ src/
 cd src
-find . -type d -exec mkdir -p ../test/{} \;
-find . -type f -not \( -iname "*.coffee" \) -exec cp -rf {} ../test/{} \;
+find . -type d -exec mkdir -p ../js/{} \;
+find . -type f -not \( -iname "*.coffee" \) -exec cp -rf {} ../js/{} \;
